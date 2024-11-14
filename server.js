6611a6b2dd9b1c0789b8e7ff935235a5da4393c2
DSRanawat7773 <dalpatsaranawat@gmail.com>
2024-11-14 14:19:50 +0530

    origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
